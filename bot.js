c539dc5a016e117223b7f2c8b0f0fd132ddcf692
Schneider Bence <beny1998@citromail.hu>
2022-06-15 17:16:56 +0200

    max player 10 setup

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const { MessageEmbed } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.DIRECT_MESSAGES] });
 var prefix = "=";
-const MAX_PLAYER = 2;
+const MAX_PLAYER = 10;
 var lobbyPlayers = [];
 var findPlayer;
 var findPlayerInFirst;
